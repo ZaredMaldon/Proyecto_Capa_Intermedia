2e8a8f83d314231e9b15358fdfc418271fa2c6fe
ZaredMaldon <zaredosvaldom@gmail.com>
2022-08-23 23:52:41 -0500

    Barra de navegacion
    Se realizo la estructura de la pagina principal

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nombre de Página</title>
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="CSS/PaginaGeneral.css" rel="stylesheet">
+    <link href="CSS/BarraDeNavegacion.css" rel="stylesheet">
+    <link href="CSS/Footer.css" rel="stylesheet">
+
+</head>
+<body>
+    <header class="header">
+        <div class="container logo-nav-container">
+            <a href="#" class="logo">LOGO</a>
+            <nav class="navigation">
+                <ul>
+                    <li><a href="#">Inicio</a></li>                  
+                    <li><a href="#">Comprar</a></li>
+                    <li><a href="#">Vender</a></li>
+                    <li><a href="#">Cotizar</a></li>
+                    <li><a href="#">Nombre</a></li>
+                    <li><a href="#">Login</a></li> 
+                </ul>
+            </nav>
+        </div>
+
+    </header>
+
+    <main class="main">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus porro laudantium suscipit iste necessitatibus voluptatem, laborum vel cumque quas fugiat, praesentium placeat inventore similique aliquid? Deserunt vel amet quia ab!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus porro laudantium suscipit iste necessitatibus voluptatem, laborum vel cumque quas fugiat, praesentium placeat inventore similique aliquid? Deserunt vel amet quia ab!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus porro laudantium suscipit iste necessitatibus voluptatem, laborum vel cumque quas fugiat, praesentium placeat inventore similique aliquid? Deserunt vel amet quia ab!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus porro laudantium suscipit iste necessitatibus voluptatem, laborum vel cumque quas fugiat, praesentium placeat inventore similique aliquid? Deserunt vel amet quia ab!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus porro laudantium suscipit iste necessitatibus voluptatem, laborum vel cumque quas fugiat, praesentium placeat inventore similique aliquid? Deserunt vel amet quia ab!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus porro laudantium suscipit iste necessitatibus voluptatem, laborum vel cumque quas fugiat, praesentium placeat inventore similique aliquid? Deserunt vel amet quia ab!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus porro laudantium suscipit iste necessitatibus voluptatem, laborum vel cumque quas fugiat, praesentium placeat inventore similique aliquid? Deserunt vel amet quia ab!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus porro laudantium suscipit iste necessitatibus voluptatem, laborum vel cumque quas fugiat, praesentium placeat inventore similique aliquid? Deserunt vel amet quia ab!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus porro laudantium suscipit iste necessitatibus voluptatem, laborum vel cumque quas fugiat, praesentium placeat inventore similique aliquid? Deserunt vel amet quia ab!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus porro laudantium suscipit iste necessitatibus voluptatem, laborum vel cumque quas fugiat, praesentium placeat inventore similique aliquid? Deserunt vel amet quia ab!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus porro laudantium suscipit iste necessitatibus voluptatem, laborum vel cumque quas fugiat, praesentium placeat inventore similique aliquid? Deserunt vel amet quia ab!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus porro laudantium suscipit iste necessitatibus voluptatem, laborum vel cumque quas fugiat, praesentium placeat inventore similique aliquid? Deserunt vel amet quia ab!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus porro laudantium suscipit iste necessitatibus voluptatem, laborum vel cumque quas fugiat, praesentium placeat inventore similique aliquid? Deserunt vel amet quia ab!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus porro laudantium suscipit iste necessitatibus voluptatem, laborum vel cumque quas fugiat, praesentium placeat inventore similique aliquid? Deserunt vel amet quia ab!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus porro laudantium suscipit iste necessitatibus voluptatem, laborum vel cumque quas fugiat, praesentium placeat inventore similique aliquid? Deserunt vel amet quia ab!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus porro laudantium suscipit iste necessitatibus voluptatem, laborum vel cumque quas fugiat, praesentium placeat inventore similique aliquid? Deserunt vel amet quia ab!</p>
+    </main>
+
+    <footer class="footer">
+        <p>Página diseñada por @Zared Maldonado y Fernanda Mondragon</p>
+    </footer>
+
+    <!-- JavaScript-->
+    <script src="Script/jquery.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+</html>
