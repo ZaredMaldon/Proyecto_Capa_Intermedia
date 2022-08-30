106074404bdc095a0d9657cc0154facf7be5e7b3
ZaredMaldon <zaredosvaldom@gmail.com>
2022-08-30 10:42:56 -0500

    Cambios Pagina completa
    Se realizo un redise;o de la pagina

--- Script/ModalLogin.js
+++ Script/ModalLogin.js
@@ -1,11 +1,6 @@
 const btnAbrirModal=document.querySelector("#Login-btn");
-const btnCerrarModal=document.querySelector("#CerrarModal-btn");
 const modal=document.querySelector("#modal");
 
 btnAbrirModal.addEventListener("click",()=>{
     modal.showModal();
-});
-
-btnCerrarModal.addEventListener("click",()=>{
-    modal.close();
 });
