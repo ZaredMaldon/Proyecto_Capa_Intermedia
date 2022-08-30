e9be93b405cfa4bf7d28120c7a698ea0a1680acc
ZaredMaldon <zaredosvaldom@gmail.com>
2022-08-30 15:32:32 -0500

    Validaciones a Login
    Se realizaron las validaciones de login

---
+++ Script/Login_Validator.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function () {
+    $(".formulario").validate({
+    
+        rules: {
+            UsuarioLogin: {
+                required:true,
+                minlength:3
+            },
+            PasswordLogin:{
+                required:true,
+                minlength:8
+            }
+        },
+        // Specify validation error messages
+        messages: {
+            UsuarioLogin: {
+                required:"Ingrese su nombre de usuario",
+                minlength:"Debe tener al menos 3 caracteres"
+            },
+            PasswordLogin:{
+                required:"Ingrese la contraseña",
+                minlength:"Ingrese al menos 8 caracteres"
+            }
+
+        }
+    
+    });
+    
+    $("#Logginbtn").click(function () { 
+        var usuario=$("UsuarioLogin").val();
+        var password=$("PasswordLogin").val();
+    });
+    });
