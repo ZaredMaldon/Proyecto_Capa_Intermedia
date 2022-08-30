185d406b215351d06bf7591bdb469db4752e1ee4
ZaredMaldon <zaredosvaldom@gmail.com>
2022-08-30 13:41:38 -0500

    Login Basico implementado
    Se realizo un login basico sin sesion aun

---
+++ Model/Login.php
@@ -0,0 +1,22 @@
+<?php
+$usuario=$_POST['UsuarioLogin'];
+$password=$_POST['PasswordLogin'];
+require_once("Conexion.php"); 
+
+$con=Conectar::conectar();
+$consulta="SELECT idUsuario,Usuario,Contrasenia FROM Usuarios where Usuario='$usuario' and Contrasenia='$password';";
+$resultado=mysqli_query($con,$consulta);
+$filas=mysqli_num_rows($resultado);
+if($filas){
+    header("location:../index.html");
+}else{
+    ?>
+    <?php
+    include("../View/Login.html");
+    ?>
+    <h1 class="bad">Error en la Autentificación</h1>
+    <?php    
+}
+mysqli_free_result($resultado);
+mysqli_close($con);
+?>
---
+++ Model/prueba.php
@@ -0,0 +1,6 @@
+
+<?php
+    require_once("Conexion.php"); 
+    $con=Conectar::conectar();
+    echo "Se realizo exitosamente la conexion a la base de datos"
+?>
