4ab9d6ddb9437173c8290e9e40c66e34ee9e7bb9
ZaredMaldon <zaredosvaldom@gmail.com>
2022-10-20 10:33:29 -0500

    cambio

--- Controller/Registro.php
+++ Controller/Registro.php
@@ -1,3 +1,29 @@
 <?php
-//Aqui va el control de registro
+include_once('Conexion.php');
+
+//Recibo todos los datos del formulario
+$Usuario = $_POST['UsuarioRegister'];
+$Email = $_POST['Email'];
+$Imagen = $_POST['ImagenUsuario'];
+$Nombres = $_POST['Nombre'];
+$APat = $_POST['APat'];
+$AMat = $_POST['ApMat'];
+$Fecha_Nacimiento = $_POST['FechaNacimiento'];
+$Sexo = $_POST['Sexo'];
+$Rol=$_POST['Rol'];
+
+$conectar = Conectar::conectar();
+
+//Usuarios
+$sql = "insert into Usuarios(Usuario,Contrasenia, Fk_Rol, Email) values ('$Usuario','$Contrasenia', '$Rol', '$Email')";
+//Persona
+$sql2 = "insert into Personas(Fk_Usuario, Imagen, Nombres, APat, AMat, Fecha_Nacimiento, Sexo, Fecha_ingreso) values ('$Fk_Usuario', '$Imagen','$Nombres', '$APat', '$AMat', '$Fecha_Nacimiento','$Sexo',NOW())";
+
+$result = $conectar->query($sql)or trigger_error("Query failed! SQL - Error: " .mysqli_error($conectar), E_USER_ERROR);
+$result2 = $conectar->query($sql2)or trigger_error("Query failed! SQL - Error: " .mysqli_error($conectar), E_USER_ERROR);
+ header("Status: 301 Moved Permanently");
+ header("Location: index.php");
+
+exit;
+
 ?>
